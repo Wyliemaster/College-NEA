c9beb77d40fdc19898bd628221b285abb686cca3
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2023-02-22 16:48:52 +0000

    User Content

---
+++ user_content/index.php
@@ -0,0 +1,57 @@
+<?php error_reporting(0); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <link rel="stylesheet" href="../frontend/css/main.css">
+    <link rel="stylesheet" href="../frontend/css/home.css">
+    <script src="../frontend/scripts/main.js"></script>
+    <script src="../frontend/scripts/prefab.js"></script>
+    <script src="../frontend/scripts/helpers.js"></script>
+
+
+</head>
+
+<body>
+    <div class="global-navbar">
+        <div>
+            <a href="../" class="global-navbar-item">Home</a>
+            <a href="./" class="global-navbar-item">User Content</a>
+        </div>
+        <div class=global-navbar-title>
+            <label>Little Man Computer Decompiler</label>
+        </div>
+
+        <?php
+
+        if (!isset($_COOKIE["LOGIN"]) && !isset($_COOKIE["NAME"])) {
+            echo '<div id="global-navbar-account">
+                <a onclick="show_login_popup()" class="global-navbar-item">Login</a>
+                <a onclick="show_register_popup();" class="global-navbar-item">Register</a>
+            </div>';
+        } else {
+            $name = $_COOKIE["NAME"] != "" ? $_COOKIE["NAME"] : "[[NAME]]";
+            echo '<div id="global-navbar-account">
+            <a onclick="show_login_popup()" class="global-navbar-item">' . $name . '\'s Code</a>
+            <a onclick="logout()" class="global-navbar-item">Logout</a>
+        </div>';
+        }
+
+        ?>
+
+    </div>
+
+
+    <script>
+        load_main_page_prefabs()
+    </script>
+
+</body>
+
+</html>
