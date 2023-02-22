ed70cbce2bd37fba13c3f9adb66a1829f7dcc567
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2023-02-22 15:13:05 +0000

    format

--- TestData/LMC_MULT.s
+++ TestData/LMC_MULT.s
@@ -1,20 +1,20 @@
-        INP
-        STA NUM_ONE
-        INP
-        STA NUM_TWO
-LOOP    LDA RESULT
-        ADD NUM_TWO
-        STA RESULT
-        LDA NUM_ONE
-        SUB ONE
-        STA NUM_ONE
-        BRP LOOP
-        LDA RESULT
-        SUB NUM_TWO
-        STA RESULT
-        OUT
-        HLT
-ONE DAT 1
-RESULT DAT 0
-NUM_ONE DAT 0
-NUM_TWO DAT 0
+                INP
+                STA NUM_ONE
+                INP
+                STA NUM_TWO
+        LOOP    LDA RESULT
+                ADD NUM_TWO
+                STA RESULT
+                LDA NUM_ONE
+                SUB ONE
+                STA NUM_ONE
+                BRP LOOP
+                LDA RESULT
+                SUB NUM_TWO
+                STA RESULT
+                OUT
+                HLT
+        ONE DAT 1
+        RESULT DAT 0
+        NUM_ONE DAT 0
+        NUM_TWO DAT 0
