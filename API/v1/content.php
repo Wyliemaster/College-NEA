3b0180f7dc88bf281a0e509fa374a717953ef084
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2023-03-06 14:35:18 +0000

    Update content.php

--- API/v1/content.php
+++ API/v1/content.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 include "../../logic/database.php";
 
 class FilterTypes
@@ -51,19 +52,22 @@ switch($filter)
     default:
         $db = db_connect();
     
-        $query = $db->prepare("SELECT 
+        $query = $db->prepare("SELECT DISTINCT
         tblcontent.content_id, 
         tblcontent.content_title, 
         tblcontent.content_description, 
         tblcontent.content_code, 
-        CASE 
-            WHEN tblratings.user_id = (SELECT tblusers.user_id FROM tblusers WHERE tblusers.user_name = :name) THEN 1 
-            ELSE 0
+        CASE
+            WHEN :name <> '' THEN (
+                SELECT count(*) 
+                FROM tblratings 
+                WHERE tblusers.user_name = :name AND tblratings.user_id = tblusers.user_id
+            )
         END AS rating_id 
         FROM tblcontent INNER JOIN tblusers ON tblcontent.user_id = tblusers.user_id 
         LEFT OUTER JOIN tblratings ON tblcontent.content_id = tblratings.content_id ORDER BY tblcontent.content_id DESC LIMIT 25;");
             
-        if($query->execute([":name" => $_COOKIE["NAME"]]))
+        if($query->execute([":name" => $_COOKIE["NAME"] ? $_COOKIE["NAME"] : ""]))
         {
             if( $data = $query->fetchAll())
             {
