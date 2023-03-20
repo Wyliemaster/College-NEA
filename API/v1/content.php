f72a1a27e893c39f0df58b1c6cea60e0ddfc60c0
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2023-03-20 16:24:30 +0000

    Update Queries + add liked filter

--- API/v1/content.php
+++ API/v1/content.php
@@ -23,18 +23,21 @@ switch($filter)
 
             $db = db_connect();
 
-            $query = $db->prepare("SELECT 
+            $query = $db->prepare("SELECT DISTINCT
             tblcontent.content_id, 
             tblcontent.content_title, 
             tblcontent.content_description, 
             tblcontent.content_code, 
-            CASE 
-                WHEN tblratings.user_id = (SELECT tblusers.user_id FROM tblusers WHERE tblusers.user_name = :name) THEN 1 
-                ELSE 0
+            CASE
+                WHEN :name <> '' THEN (
+                    SELECT count(*) 
+                    FROM tblratings 
+                    WHERE tblusers.user_name = :name AND tblratings.user_id = tblusers.user_id
+                )
             END AS rating_id 
             FROM tblcontent INNER JOIN tblusers ON tblcontent.user_id = tblusers.user_id 
             LEFT OUTER JOIN tblratings ON tblcontent.content_id = tblratings.content_id
-            WHERE tblusers.user_name = :name ORDER BY tblcontent.content_id DESC LIMIT 25");
+            WHERE tblusers.user_name = :name ORDER BY tblcontent.content_id DESC LIMIT 25;");
             
             if($query->execute([":name" => $name]))
             {
@@ -48,7 +51,34 @@ switch($filter)
         $db = NULL;
         break;
 
-    // fetch content default
+    case FilterTypes::MOST_LIKES:
+        $db = db_connect();
+    
+        $query = $db->prepare("SELECT DISTINCT
+        tblcontent.content_id, 
+        tblcontent.content_title, 
+        tblcontent.content_description, 
+        tblcontent.content_code, 
+        CASE
+            WHEN :name <> '' THEN (
+                SELECT count(*) 
+                FROM tblratings 
+                WHERE tblusers.user_name = :name AND tblratings.user_id = tblusers.user_id
+            )
+        END AS rating_id 
+        FROM tblcontent INNER JOIN tblusers ON tblcontent.user_id = tblusers.user_id 
+        LEFT OUTER JOIN tblratings ON tblcontent.content_id = tblratings.content_id GROUP BY tblratings.content_id ORDER BY count(tblratings.content_id) DESC LIMIT 25;");
+            
+        if($query->execute([":name" => $_COOKIE["NAME"] ? $_COOKIE["NAME"] : ""]))
+        {
+            if( $data = $query->fetchAll())
+            {
+                echo json_encode($data);
+            }
+        }
+        $query = NULL;
+        $db = NULL;
+        break;
     default:
         $db = db_connect();
     
