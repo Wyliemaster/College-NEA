10ca9a30cf5bf01c05753d5c1fbfa2f9a7fe388e
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2023-03-20 15:49:10 +0000

    Update login.php

--- API/v1/login.php
+++ API/v1/login.php
@@ -11,13 +11,17 @@ if (!empty($name) && !empty($pass)) {
     // Ensuring the username is the correct size
     if (strlen($name) < 4)
     {
-        exit("Username too short");
+        // Creates alert saying "Login Error: Username is too short. Please make sure your username is atleast 4 characters long"
+        header("Location: ../../?ERROR=Login%20Error%3A%20Username%20is%20too%20short.%20Please%20make%20sure%20your%20username%20is%20atleast%204%20characters%20long");
+        exit("REDIRECT_ERROR: <a href='../../'>Return to home page</a>"); // In the event that the redirect doesn't work
     }
     // Forcing password to be atleast 7 characters long
     // for the sake of keeping their account secure
     if (strlen($pass) < 8)
     {
-        exit("Password too short");
+        // Creates alert saying "Login Error: Password too short. Please make sure your password is atleast 8 characters long"
+        header("Location: ../../?ERROR=Login%20Error%3A%20Password%20too%20short.%20Please%20make%20sure%20your%20password%20is%20atleast%208%20characters%20long");
+        exit("REDIRECT_ERROR: <a href='../../'>Return to home page</a>"); // In the event that the redirect doesn't work
     }
 
     $db = db_connect();
@@ -32,14 +36,23 @@ if (!empty($name) && !empty($pass)) {
                 setcookie("NAME", $name, $time + (86400 * 30), "/");
 
                 $db = NULL;
-                exit("$name is logged in <br><a href='../../'>Return to home page</a>");
+                header("Location: ../../index.php");
+                exit("REDIRECT_ERROR: <a href='../../'>Return to home page</a>"); // In the event that the redirect doesn't work
             }
         }
         $db = NULL;
-        exit("Unable to find user");
+        // creates alert saying "Login Error: Cannot Find User"
+        header("Location: ../../?ERROR=Login%20Error%3A%20Cannot%20Find%20User");
+        exit("REDIRECT_ERROR: <a href='../../'>Return to home page</a>"); // In the event that the redirect doesn't work
     }
 
     $db = NULL;
-    exit();
+
+    // creates alert saying "Login Error: Query failed. Please try again"
+    header("Location: ../../?ERROR=Login%20Error%3A%20Query%20failed.%20Please%20try%again");
+    exit("REDIRECT_ERROR: <a href='../../'>Return to home page</a>"); // In the event that the redirect doesn't work
 }
-exit("Please Provide Login Details");
+
+// creates alert saying "Login Error: Please Provide Login Details"
+header("Location: ../../?ERROR=Login%20Error%3A%20Please%20Provide%20Login%20Details");
+exit("REDIRECT_ERROR: <a href='../../'>Return to home page</a>"); // In the event that the redirect doesn't work
